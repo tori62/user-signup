944a5b37b769b1a0c326cce3fc6ecc39a86b6e82
Tori Gonzalez <tori@nexmatix.com>
2018-04-03 23:42:05 -0500

    working on program

--- main.py
+++ main.py
@@ -5,8 +5,32 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
+
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/welcome", methods=['POST'])
+def response():
+    name = request.form['username']
+    word = request.form['password']
+    verify = request.form['verify_password']
+    
+    if name == "":
+        name_error = "Please enter a username"
+        return render_template('index.html' password=word verify_password= verify name_error=name_error)
+
+    elif word == "":
+        word_error = "Please enter a password"
+        return render_template('index.html' password=word_error)
+
+    elif verify == "":
+        verify_error = "Please verify password"
+        return render_template('index.html' verify_password=verify_error)
+
+    else:
+        return render_template('welcome.html', username=name)
+
+
 
 app.run()
