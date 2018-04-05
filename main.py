75ded63c1e225e488131854270467019e7134197
Tori Gonzalez <tori@nexmatix.com>
2018-04-05 16:15:32 -0500

    code with all validation except email

--- main.py
+++ main.py
@@ -20,17 +20,42 @@ def response():
         error = "Please enter a username"
         username=''
         return render_template('index.html', username=username, name_error=error)
-
-    elif word == "":
+    else:
+        if not_valid(name) == True:
+            error = "Please enter a valid username (between 3-20 characters and no spaces)"
+            username=''
+            return render_template('index.html', username=username, name_error=error)  
+    
+    if word == "":
         error = "Please enter a password"
         return render_template('index.html', username=name, word_error=error)
+    else:
+        if not_valid(word) == True:
+            error = "Please enter a valid password between 3-20 characters and no spaces"
+            password = ''
+            return render_template('index.html',username=name, password=password, word_error=error)
 
-    elif verify != word:
-        error = "The password does not match, please re-enter"
-        return render_template('index.html', username=name, password=word, verify_error=error)
+    if verify != word:
+        error = "Password did not match. Please re-enter password and verify"
+        return render_template('index.html', username=name, verify_error=error)
 
     else:
         return render_template('welcome.html', username=name)
 
+def not_valid(name):
+    for i in name:
+        if i == " ":
+            return True
+        elif len(name) < 3 or len(name) > 20:
+            return True
+        else:
+            return False
 
 app.run()
+
+
+#    else:
+#        if not_valid(name) == True:
+#            error = "Please enter a valid username (between 3-20 characters and no spaces)"
+#            username=''
+#            return render_template('index.html', username=username, name_error=error)
